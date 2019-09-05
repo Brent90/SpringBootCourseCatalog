91353b465f793f1f16cd23a2cd113b3fdf2059c4
brent90 <brentjbaumgart@gmail.com>
2019-09-05 18:05:55 -0500

    first commit

---
+++ src/main/java/com/slinger/SpringBootCourseCatalog/SpringBootCourseCatalogApplication.java
@@ -0,0 +1,13 @@
+package com.slinger.SpringBootCourseCatalog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootCourseCatalogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootCourseCatalogApplication.class, args);
+	}
+
+}
